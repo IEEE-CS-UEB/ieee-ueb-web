d078329a3d6666399db44b4f58eb0819f7326c33
Juan Castillo <cfuendesign@tutanota.com>
2023-09-18 17:38:37 -0500

    :sparkles: discover routing (ESTA SEMANA LO TERMINO)

--- scripts/static-admin-express/src/server/main.ts
+++ scripts/static-admin-express/src/server/main.ts
@@ -37,6 +37,38 @@ app.post('/engineers/update', (req, res) => {
   });
 });
 
+app.post('/societies/update', (req, res) => {
+  // Obtener los datos enviados en el cuerpo de la solicitud
+  const newData = req.body;
+
+  // Leer el archivo JSON existente
+  fs.readFile('./public/societies.json', 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'No se pudo leer el archivo de datos.' });
+    }
+
+    try {
+      // Escribir los datos actualizados en el archivo JSON
+      fs.writeFile('./public/societies.json', JSON.stringify(newData), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'No se pudo escribir en el archivo de datos.' });
+        }
+
+        // Devolver una respuesta exitosa
+        return res.json({ message: 'Melos' });
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'No se pudo analizar el archivo de datos JSON.' });
+    }
+
+    return null;
+  });
+});
+
+
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Vite + Express activo en el 3000...")
